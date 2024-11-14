e3292f621789dc3e2ae1b397f8d8707abcccc1ae
ViacheslavAnisimov <forvsn@gmail.com>
2024-11-14 06:24:19 -0500

    feat: add Home.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
